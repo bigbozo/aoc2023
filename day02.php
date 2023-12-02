b9612745a467aaf23802399edda01220eb1a0cf7
Bugi Goertz <burkhard.goertz@shapefruit.de>
2023-12-02 19:04:51 +0100

    solves day 2 part 1

--- day02.php
+++ day02.php
@@ -15,7 +15,38 @@ class CubeDraw
 
     public static function fromTest($test): CubeDraw
     {
-        return new static((int)$test['red'] ?? 0, (int)$test['green'] ?? 0, (int)$test['blue'] ?? 0);
+        return new static(
+            (int)($test['red'] ?? 0),
+            (int)($test['green'] ?? 0),
+            (int)($test['blue'] ?? 0)
+        );
+    }
+}
+
+class Game
+{
+
+    /**
+     * @var CubeDraw[]
+     */
+    public array $draws = [];
+
+    /**
+     * @param CubeDraw[] $draws
+     */
+    public function __construct(array $draws)
+    {
+        $this->draws = $draws;
+    }
+
+    public function isValid($red = 0, $green = 0, $blue = 0)
+    {
+        foreach ($this->draws as $draw) {
+            if ($draw->red > $red) return false;
+            if ($draw->green > $green) return false;
+            if ($draw->blue > $blue) return false;
+        }
+        return  true;
     }
 }
 
@@ -24,7 +55,7 @@ function parseData($data)
     $games = [];
     foreach ($data as $line) {
         if (preg_match('/Game\s(\d+):\s(.*)$/', chop($line), $match)) {
-            $games[$match[1]] = array_reduce(explode('; ', $match[2]), function ($carry, $subset) {
+            $games[$match[1]] = new Game(array_reduce(explode('; ', $match[2]), function ($carry, $subset) {
                 $draw = [];
                 $colorCounts = explode(', ', $subset);
                 foreach ($colorCounts as $colorCount) {
@@ -33,12 +64,19 @@ function parseData($data)
                 }
                 $carry[] = CubeDraw::fromTest($draw);
                 return $carry;
-            }, []);
+            }, []));
         };
     }
     return $games;
 }
 
-$data = parseData(file('input/day2-test.txt'));
+$games = parseData(file('input/day2.txt'));
 
-var_export($data);
+$score = 0;
+foreach ($games as $id => $game) {
+    /** @var Game $game */
+    if ($game->isValid(red: 12, green: 13, blue: 14)) {
+        $score += $id;
+    }
+}
+echo $score;
