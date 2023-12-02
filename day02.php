fbace7596bd5fd37844d24d0e722964d1c57ad72
Bugi Goertz <burkhard.goertz@shapefruit.de>
2023-12-02 19:15:34 +0100

    solves day 2 part 2

--- day02.php
+++ day02.php
@@ -6,7 +6,7 @@ class CubeDraw
     public int $green = 0;
     public int $blue = 0;
 
-    public function __construct($red, $green, $blue)
+    public function __construct($red = 0, $green = 0, $blue = 0)
     {
         $this->red = $red;
         $this->green = $green;
@@ -20,6 +20,17 @@ class CubeDraw
             (int)($test['green'] ?? 0),
             (int)($test['blue'] ?? 0)
         );
+    }
+
+    public function power()
+    {
+        return $this->red * $this->green * $this->blue;
+    }
+
+    public function riseMax(CubeDraw $draw) {
+        $this->red = max($this->red,$draw->red);
+        $this->green = max($this->green,$draw->green);
+        $this->blue = max($this->blue,$draw->blue);
     }
 }
 
@@ -46,7 +57,16 @@ class Game
             if ($draw->green > $green) return false;
             if ($draw->blue > $blue) return false;
         }
-        return  true;
+        return true;
+    }
+
+    public function getMinCubes()
+    {
+        return array_reduce($this->draws, function ($carry, $draw) {
+            $carry->riseMax($draw);
+            return $carry;
+        }, new CubeDraw());
+
     }
 }
 
@@ -72,11 +92,18 @@ function parseData($data)
 
 $games = parseData(file('input/day2.txt'));
 
-$score = 0;
+$setPowerSum =0;
 foreach ($games as $id => $game) {
     /** @var Game $game */
     if ($game->isValid(red: 12, green: 13, blue: 14)) {
         $score += $id;
     }
+    $setPowerSum += $game->getMinCubes()->power();
 }
-echo $score;
+
+echo "Day 2 - Part 1: The score is " . $score . PHP_EOL;
+
+echo "Day 2 - Part 2: Set-Power-Sum is ". $setPowerSum;
+
+
+
