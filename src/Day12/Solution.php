3afcf5e712dbbfcbd17aafe828de5c6a641a1f36
Bugi Goertz <burkhard.goertz@shapefruit.de>
2023-12-13 10:10:42 +0100

    solves day12 - acceptable time
    bin/aoc run 12  0.73s user 0.03s system 90% cpu 0.837 total

--- src/Day12/Solution.php
+++ src/Day12/Solution.php
@@ -8,6 +8,8 @@ use Bizbozo\Adventofcode2023\Solutions\UnitResult;
 
 class Solution implements SolutionInterface
 {
+    static $cache = [];
+    private static $resultFile = __DIR__ . '/../../output/day12-cached.txt';
 
     /**
      * @param array $lines
@@ -29,16 +31,13 @@ class Solution implements SolutionInterface
     {
 
         $records = static::parseData(explode(PHP_EOL, $inputStream));
+
         $sum = array_sum(array_map(function ($record) use ($records) {
-
             return static::countVariants($record['pattern'], $record['numbers']);
-
         }, $records));
 
         $sum2 = array_sum(array_map(function ($record) use ($records) {
-
-            return static::countVariants(...static::foldOut($record['pattern'],$record['numbers']));
-
+            return static::countVariants(...static::foldOut($record['pattern'], $record['numbers']));
         }, $records));
 
 
@@ -52,33 +51,89 @@ class Solution implements SolutionInterface
     private static function countVariants(mixed $pattern, mixed $numbers): int
     {
 
-        if (!count($numbers)) {
-            if (str_contains($pattern, '#')) return 0;
-            return 1;
-        }
-        $counter = 0;
-        $nextChar = substr($pattern, 0, 1);
-        if ($nextChar === '.' || $nextChar === '?') {
-            $counter += self::countVariants(substr($pattern, 1), $numbers);
-        }
-        if (($nextChar === '#' || $nextChar === '?')
-            && strlen($pattern) >= $numbers[0]
-            && !str_contains(substr($pattern, 0, $numbers[0]), '.')
-            && (strlen($pattern) == $numbers[0] || substr($pattern, $numbers[0], 1) != '#')
-        ) {
+        $key = self::getKey($pattern, $numbers);
+        if (isset(static::$cache[$key])) return static::$cache[$key];
+        $reverseKey = static::reverseKey($pattern, $numbers);
+        if (isset(static::$cache[$reverseKey])) return static::$cache[$reverseKey];
 
-            $counter += self::countVariants(substr($pattern, $numbers[0] + 1), array_slice($numbers, 1));
+        if (!count($numbers)) {
+            if (str_contains($pattern, '#')) {
+                $counter = 0;
+            } else {
+                $counter = 1;
+            }
+        } else {
+
+            $counter = 0;
+            $nextChar = substr($pattern, 0, 1);
+            if ($nextChar === '.' || $nextChar === '?') {
+                $counter += self::countVariants(substr($pattern, 1), $numbers);
+            }
+            if (($nextChar === '#' || $nextChar === '?')
+                && strlen($pattern) >= $numbers[0]
+                && !str_contains(substr($pattern, 0, $numbers[0]), '.')
+                && (strlen($pattern) == $numbers[0] || substr($pattern, $numbers[0], 1) != '#')
+            ) {
+                $counter += self::countVariants(substr($pattern, $numbers[0] + 1), array_slice($numbers, 1));
+            }
         }
+
+        static::$cache[$key] = $counter;
+
         return $counter;
+    }
+
+    private static function needed($numbers)
+    {
+        return array_sum($numbers) + count($numbers) - 1;
     }
 
     private static function foldOut(string $pattern, array $numbers)
     {
         return [
-            implode("?", array_fill(0,5,$pattern)),
-            array_merge($numbers,$numbers,$numbers,$numbers,$numbers)
+            implode("?", array_fill(0, 5, $pattern)),
+            array_merge($numbers, $numbers, $numbers, $numbers, $numbers)
         ];
 
+    }
+
+    /**
+     * @param mixed $pattern
+     * @param mixed $numbers
+     * @return string
+     */
+    private static function getKey(mixed $pattern, mixed $numbers): string
+    {
+        $key = md5($pattern . '-' . implode('-', $numbers));
+        return $key;
+    }
+
+    private static function reverseKey(string $pattern, array $numbers): string
+    {
+        $key = md5(strrev($pattern) . '-' . strrev(implode('-', $numbers)));
+        return $key;
+    }
+
+    private static function getCachedResults()
+    {
+        $cachedResults = [];
+
+        if (file_exists(static::$resultFile)) {
+            $lines = file_get_contents(static::$resultFile);
+            foreach (explode(PHP_EOL, $lines) as $line) {
+                if (trim($line)) {
+                    list($key, $val) = explode(":", $line);
+                    $cachedResults[$key] = $val;
+                }
+            }
+        }
+        return $cachedResults;
+    }
+
+    private static function cacheResult(string $key, int $result)
+    {
+
+        file_put_contents(static::$resultFile, "$key:$result" . PHP_EOL, FILE_APPEND);
     }
 
 
